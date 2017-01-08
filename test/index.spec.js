cf73c9c9c15c534c2ca5b442e6816f9ebe421197
Joseph W Becher <jwbecher@drazisil.com>
2017-01-08 10:54:30 -0500

    can correctly merge files

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,28 +1,47 @@
 const util = require('util')
-var assert = require('assert')
 var junitMerge = require('../lib/index.js')
 var should = require('chai').should() // eslint-disable-line no-unused-vars
 
 util.inspect.defaultOptions.depth = null
 
-describe('Array', function () {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal(-1, [1, 2, 3].indexOf(4))
+describe('File Handling', function () {
+  describe('listXmlFiles()', function () {
+    it('should return 3 files', function () {
+      console.log('==' + junitMerge.listXmlFiles('test/fixtures'))
+      junitMerge.listXmlFiles('test/fixtures').length.should.equal(3)
+    })
+
+    it('should report not a valid directory', function () {
+      junitMerge.listXmlFiles('bad_dir').should.equal('Bad directory')
+    })
+
+    it('should report not xml files found', function () {
+      junitMerge.listXmlFiles('lib').should.equal('No xml files found')
+    })
+
+    it('should cause an unknown error', function () {
+      junitMerge.listXmlFiles('/var/root')
+      'No xml files found'.should.equal('No xml files found')
     })
   })
 })
 
 describe('XML Handling', function () {
-  describe('parseXmlFromFile()', function () {
-    it('should return 6 total tests 1', function () {
-      junitMerge.parseXmlFromFile('./test/fixtures/1.xml')
+  describe('getTestsuites()', function () {
+    it('should return 1 testsuites', function () {
+      junitMerge.getTestsuites('./test/fixtures/1.xml')
       junitMerge.testsuiteCount.should.equal(1)
     })
 
-    it('should return 6 total tests 2', function () {
-      junitMerge.parseXmlFromFile('./test/fixtures/3.xml')
+    it('should return 2 testsuites', function () {
+      junitMerge.getTestsuites('./test/fixtures/3.xml')
       junitMerge.testsuiteCount.should.equal(2)
+    })
+  })
+
+  describe('getTestsuites()', function () {
+    it('should return merged xml file', function () {
+      junitMerge.mergeFiles(['./test/fixtures/1.xml', './test/fixtures/3.xml']).length.should.equal(1077)
     })
   })
 })
