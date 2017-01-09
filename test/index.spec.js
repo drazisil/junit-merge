e7fbafb2e0fceeec4fc2b67ed6c4d9b8c760ba50
Joseph W Becher <jwbecher@drazisil.com>
2017-01-09 13:16:11 -0500

    added missing error handling paths

--- test/index.spec.js
+++ test/index.spec.js
@@ -7,20 +7,44 @@ util.inspect.defaultOptions.depth = null
 describe('File Handling', function () {
   describe('listXmlFiles()', function () {
     it('should return 3 files', function () {
-      junitMerge.listXmlFiles('test/fixtures').length.should.equal(3)
+      junitMerge.listXmlFiles('test/fixtures', function (err, res) {
+        if (err) {
+          throw err
+        } else {
+          res.length.should.equal(4)
+        }
+      })
     })
 
     it('should report not a valid directory', function () {
-      junitMerge.listXmlFiles('bad_dir').should.equal('Bad directory')
+      junitMerge.listXmlFiles('bad_dir', function (err, res) {
+        if (err) {
+          err.should.equal('Bad directory')
+        } else {
+          res.should.equal('This should error')
+        }
+      })
     })
 
     it('should report not xml files found', function () {
-      junitMerge.listXmlFiles('lib').should.equal('No xml files found')
+      junitMerge.listXmlFiles('lib', function (err, res) {
+        if (err) {
+          console.log(err)
+          err.should.equal('No xml files found')
+        } else {
+          res.should.equal('This should error')
+        }
+      })
     })
 
-    it('should cause an unknown error', function () {
-      junitMerge.listXmlFiles('/var/root')
-      'No xml files found'.should.equal('No xml files found')
+    it('should cause an permission denied error', function () {
+      junitMerge.listXmlFiles('/var/root', function (err, res) {
+        if (err) {
+          err.code.should.equal('EACCES')
+        } else {
+          res.should.equal('This should error')
+        }
+      })
     })
   })
 })
@@ -28,19 +52,60 @@ describe('File Handling', function () {
 describe('XML Handling', function () {
   describe('getTestsuites()', function () {
     it('should return 1 testsuites', function () {
-      junitMerge.getTestsuites('./test/fixtures/1.xml')
-      junitMerge.testsuiteCount.should.equal(1)
+      junitMerge.getTestsuites('./test/fixtures/1.xml', function (err, res) {
+        if (err) {
+          throw err
+        }
+        junitMerge.testsuiteCount.should.equal(1)
+      })
     })
 
     it('should return 2 testsuites', function () {
-      junitMerge.getTestsuites('./test/fixtures/3.xml')
-      junitMerge.testsuiteCount.should.equal(2)
+      junitMerge.getTestsuites('./test/fixtures/3.xml', function (err, res) {
+        if (err) {
+          throw err
+        }
+        junitMerge.testsuiteCount.should.equal(2)
+      })
     })
-  })
 
-  describe('getTestsuites()', function () {
+    it('should return No tests found', function () {
+      junitMerge.mergeFiles(['./test/fixtures/empty.xml'], function (err, res) {
+        if (err) {
+          err.should.equal('No tests found')
+        } else {
+          res.should.equal('This should error')
+        }
+      })
+    })
+
     it('should return merged xml file', function () {
-      junitMerge.mergeFiles(['./test/fixtures/1.xml', './test/fixtures/3.xml']).length.should.equal(1077)
+      junitMerge.mergeFiles(['./test/fixtures/1.xml', './test/fixtures/3.xml'], function (err, res) {
+        if (err) {
+          throw err
+        }
+        res.length.should.equal(1077)
+      })
+    })
+
+    it('should return No testsuites found', function () {
+      junitMerge.getTestsuites('./test/fixtures/12.xml', function (err, res) {
+        if (err) {
+          err.should.equal('File not found')
+        } else {
+          res.should.equal(null)
+        }
+      })
+    })
+
+    it('should return Bad directory', function () {
+      junitMerge.listXmlFiles('./moo', function (err, res) {
+        if (err) {
+          err.should.equal('Bad directory')
+        } else {
+          res.should.equal(null)
+        }
+      })
     })
   })
 })
