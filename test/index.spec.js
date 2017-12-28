5a4356175a0140d545291969636709ac99fe5d27
Joseph W Becher <drazisil@users.noreply.github.com>
2017-12-28 10:10:47 -0500

    Switch to sync instead of async (#25)
    * Converted listXmlFiles to promises
    
    * No promises, make everything sync
    
    * Corrected the CLI bin
    
    * Corrected CLI, again
    
    * Removed async wait

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,58 +1,38 @@
-var junitMerge = require('../lib/index.js')
-var should = require('chai').should() // eslint-disable-line no-unused-vars
-var expect = require('chai').expect // eslint-disable-line no-unused-vars
+var junitMerge = require("../lib/index.js");
+var should = require("chai").should(); // eslint-disable-line no-unused-vars
+var expect = require("chai").expect; // eslint-disable-line no-unused-vars
 
-describe('File Handling', function () {
-  describe('listXmlFiles()', function () {
-    it('should return 5 files', function () {
-      junitMerge.listXmlFiles('test/fixtures', false, function (err, res) {
-        if (err) {
-          throw err
-        } else {
-          res.length.should.equal(5)
-        }
-      })
-    })
+describe("File Handling", function() {
+  describe("listXmlFiles()", function() {
+    it("should return 5 files", function() {
+      junitMerge.listXmlFiles("test/fixtures").length.should.equal(5);
+    });
+  });
 
-    it('should return 7 files', function () {
-      junitMerge.listXmlFiles('test/fixtures', true, function (err, res) {
-        if (err) {
-          throw err
-        } else {
-          res.length.should.equal(7)
-        }
-      })
-    })
+  it("should return 7 files", function() {
+    junitMerge.listXmlFiles("test/fixtures", true).length.should.equal(7);
+  });
+});
 
-    it('should report not a valid directory', function () {
-      junitMerge.listXmlFiles('bad_dir', false, function (err, res) {
-        if (err) {
-          err.code.should.equal('ENOENT')
-        } else {
-          res.should.equal('This should error')
-        }
-      })
-    })
+it("should report not a valid directory", function() {
+  try {
+    junitMerge.listXmlFiles("bad_dir", false);
+  } catch (e) {
+    e.code.should.equal("ENOENT");
+  }
+});
 
-    it('should report not xml files found', function () {
-      junitMerge.listXmlFiles('lib', false, function (err, res) {
-        if (err) {
-          err.should.equal('No xml files found')
-        } else {
-          res.should.equal('This should error')
-        }
-      })
-    })
-  })
+it("should report not xml files found", function() {
+  junitMerge
+    .listXmlFiles("lib", false)
+    .message.should.equal("No xml files found");
+});
 
-  it('should be able to write a file', function (done) {
-    junitMerge.writeMergedFile('test/moo.txt', 'moo', function (err, res) {
-      if (err) {
-        err.should.equal('This should not error')
-      } else {
-        expect('everything').to.be.ok
-        done()
-      }
-    })
-  })
-})
+it("should be able to write a file", function() {
+  try {
+    junitMerge.writeMergedFile("test/moo.txt", "moo");
+    expect("everything").to.be.ok;
+  } catch (err) {
+    err.should.equal("This should not error");
+  }
+});
