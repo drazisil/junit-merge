76204c877171617eb3ae1b21e1726e165ac268d7
Joseph W Becher <jwbecher@drazisil.com>
2017-12-28 09:50:20 -0500

    No promises, make everything sync

--- test/index.spec.js
+++ test/index.spec.js
@@ -5,48 +5,44 @@ var expect = require("chai").expect; // eslint-disable-line no-unused-vars
 describe("File Handling", function() {
   describe("listXmlFiles()", function() {
     it("should return 5 files", function() {
-      junitMerge.listXmlFiles("test/fixtures").then(res => {
-        res.length.should.equal(5);
-      });
-    });
-
-    it("should return 7 files", function() {
-      junitMerge.listXmlFiles("test/fixtures", true).then(res => {
-        res.length.should.equal(7);
-        })
-      });
-
-    it("should report not a valid directory", function() {
-      junitMerge.listXmlFiles("bad_dir", false).catch(err => {
-        err.code.should.equal("ENOENT");
-      });
-    });
-
-    it("should report not xml files found", function() {
-      junitMerge.listXmlFiles("lib", false).catch(err => {
-          err.message.should.equal("No xml files found");
-      });
+      junitMerge.listXmlFiles("test/fixtures").length.should.equal(5);
     });
   });
 
-  it("should be able to write a file", function(done) {
-    junitMerge.writeMergedFile("test/moo.txt", "moo", function(err, res) {
-      if (err) {
-        err.should.equal("This should not error");
-      } else {
-        expect("everything").to.be.ok;
-        done();
-      }
-    });
+  it("should return 7 files", function() {
+    junitMerge.listXmlFiles("test/fixtures", true).length.should.equal(7);
+  });
+});
+
+it("should report not a valid directory", function() {
+  try {
+    junitMerge.listXmlFiles("bad_dir", false);
+  } catch (e) {
+    e.code.should.equal("ENOENT");
+  }
+});
+
+it("should report not xml files found", function() {
+  junitMerge
+    .listXmlFiles("lib", false)
+    .message.should.equal("No xml files found");
+});
+
+it("should be able to write a file", function(done) {
+  junitMerge.writeMergedFile("test/moo.txt", "moo", function(err, res) {
+    if (err) {
+      err.should.equal("This should not error");
+    } else {
+      expect("everything").to.be.ok;
+      done();
+    }
   });
 });
 
 describe("File Handling - Promises", function() {
   describe("listXmlFiles()", function() {
     it("should return 5 files", function() {
-      junitMerge.listXmlFiles("test/fixtures").then(res => {
-        res.length.should.equal(5);
-      });
+      junitMerge.listXmlFiles("test/fixtures").length.should.equal(5);
     });
   });
 });
--- test/xml.spec.js
+++ test/xml.spec.js
@@ -1,70 +1,55 @@
-var junitMerge = require('../lib/index.js')
-var should = require('chai').should() // eslint-disable-line no-unused-vars
+var junitMerge = require("../lib/index.js");
+var should = require("chai").should(); // eslint-disable-line no-unused-vars
 
-describe('XML Handling', function () {
-  describe('getTestsuites()', function () {
-    it('should  return a testsuite', function (done) {
-      junitMerge.getTestsuites('./test/fixtures/1.xml', function (err, res) {
-        if (err) { throw err }
-        res.should.have.string('testsuite name')
-        done()
-      })
-    })
+describe("XML Handling", function() {
+  describe("getTestsuites()", function() {
+    it("should return a testsuite", function() {
+      junitMerge
+        .getTestsuites("./test/fixtures/1.xml")
+        .should.have.string("testsuite name");
+    });
 
-    it('should return 1 testsuites', function () {
-      junitMerge.getTestsuites('./test/fixtures/1.xml', function (err, res) {
-        if (err) {
-          throw err
-        }
-        junitMerge.testsuiteCount.should.equal(1)
-      })
-    })
+    it("should return 1 testsuites", function() {
+      junitMerge.getTestsuites("./test/fixtures/1.xml").should.be.a("string");
+      junitMerge.testsuiteCount.should.equal(1);
+    });
 
-    it('should return 1 testsuite', function () {
-      junitMerge.getTestsuites('./test/fixtures/testcase-1.xml', function (err, res) {
-        if (err) {
-          throw err
-        }
-        junitMerge.testsuiteCount.should.equal(1)
-      })
-    })
+    it("should return 1 testsuite", function() {
+      junitMerge
+        .getTestsuites("./test/fixtures/testcase-1.xml")
+        .should.be.a("string");
+      junitMerge.testsuiteCount.should.equal(1);
+    });
 
-    it('should return 2 testsuites', function () {
-      junitMerge.getTestsuites('./test/fixtures/3.xml', function (err, res) {
-        if (err) {
-          throw err
-        }
-        junitMerge.testsuiteCount.should.equal(2)
-      })
-    })
+    it("should return 2 testsuites", function() {
+      junitMerge.getTestsuites("./test/fixtures/3.xml").should.be.a("string");
+      junitMerge.testsuiteCount.should.equal(2);
+    });
 
-    it('should return No tests found', function () {
-      junitMerge.mergeFiles(['./test/fixtures/empty.xml'], function (err, res) {
-        if (err) {
-          err.should.equal('No tests found')
-        } else {
-          res.should.equal('This should error')
-        }
-      })
-    })
+    it("should return No testsuites found", function() {
+      try {
+        junitMerge.getTestsuites("./test/fixtures/12.xml");
+      } catch (err) {
+        err.message.should.equal("File not found");
+      }
+    });
+  });
+});
 
-    it('should return merged xml file', function () {
-      junitMerge.mergeFiles(['./test/fixtures/1.xml', './test/fixtures/3.xml'], function (err, res) {
-        if (err) {
-          throw err
-        }
-        res.length.should.equal(1077)
-      })
-    })
+describe("File Handling", function() {
+  describe("mergeFiles()", function() {
+    it("should return No tests found", function() {
+      try {
+        junitMerge.mergeFiles(["./test/fixtures/empty.xml"]);
+      } catch (err) {
+        err.message.should.equal("No tests found");
+      }
+    });
 
-    it('should return No testsuites found', function () {
-      junitMerge.getTestsuites('./test/fixtures/12.xml', function (err, res) {
-        if (err) {
-          err.should.equal('File not found')
-        } else {
-          res.should.equal(null)
-        }
-      })
-    })
-  })
-})
+    it("should return merged xml file", function() {
+      junitMerge
+        .mergeFiles(["./test/fixtures/1.xml", "./test/fixtures/3.xml"])
+        .length.should.equal(1077);
+    });
+  });
+});
