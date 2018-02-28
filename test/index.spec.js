d89a4acbf66faf8edc5cb64b5485ca7ec0948003
Joe Becher <drazisil@users.noreply.github.com>
2018-02-28 11:13:12 -0500

    v1.3.0 (#36)
    * 1.3.0
    * Add support for creating missing output directory #35

--- test/index.spec.js
+++ test/index.spec.js
@@ -14,27 +14,44 @@ describe("File Handling", function() {
   it("should return 7 files", function() {
     junitMerge.listXmlFiles("test/fixtures", true).length.should.equal(7);
   });
-});
 
-it("should report not a valid directory", function() {
-  try {
-    junitMerge.listXmlFiles("bad_dir", false);
-  } catch (e) {
-    e.code.should.equal("ENOENT");
-  }
-});
+  it("should report not a valid directory", function() {
+    try {
+      junitMerge.listXmlFiles("bad_dir", false);
+    } catch (e) {
+      e.code.should.equal("ENOENT");
+    }
+  });
 
-it("should report not xml files found", function() {
-  junitMerge
-    .listXmlFiles("lib", false)
-    .message.should.equal("No xml files found");
-});
+  it("should report not xml files found", function() {
+    junitMerge
+      .listXmlFiles("lib", false)
+      .message.should.equal("No xml files found");
+  });
 
-it("should be able to write a file", function() {
-  try {
-    junitMerge.writeMergedFile("test/moo.txt", "moo");
-    expect("everything").to.be.ok;
-  } catch (err) {
-    err.should.equal("This should not error");
-  }
+  it("should be able to write a file", function() {
+    try {
+      junitMerge.writeMergedFile("test/moo.txt", "moo");
+      expect("everything").to.be.ok;
+    } catch (err) {
+      err.should.equal("This should not error");
+    }
+  });
+
+  it("should not be able to write a file if the output dir does not exist", function() {
+    try {
+      junitMerge.writeMergedFile("spacegoats/moo.txt", "moo");
+    } catch (err) {
+      err.message.should.equal("Missing output directory");
+    }
+  });
+
+  it("should be able to write a file if the output dir does not exist and createDir is true", function() {
+    try {
+      junitMerge.writeMergedFile("goats/moo.txt", "moo", true);
+      expect("everything").to.be.ok;
+    } catch (err) {
+      err.should.equal("This should not error");
+    }
+  });
 });
